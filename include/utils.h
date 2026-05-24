0c13da78729464175323bab06ca802e827c7c907
Fernanda Neves <fernanda_neves@ufms.br>
2026-05-23 22:18:49 -0400

    Update utils.h

--- include/utils.h
+++ include/utils.h
@@ -5,13 +5,13 @@
 using namespace std;
 
 
-
 // Gera uma matriz diagonal dominante
 void gerar_matriz(vector<vector<double>>& A, vector<double>& b, int n);
 
 
 // Converte matriz 2D para vetor 1D
-void converter_matriz_para_vetor(vector<vector<double>>& A, double *h_A);
+void converter_matriz_para_vetor(vector<vector<double>>& A, vector<double>& b,
+                                double *h_A, double *h_b, int n);
 
 
 // Calcula diferença entre resultado CPU e GPU
