7f5b6b145bfb39ecd7c96886954c955a2430b412
Fernanda Neves <fernanda_neves@ufms.br>
2026-05-23 22:06:28 -0400

    Update main.cpp
    Tirei a descrição das funções da main já que agora isso é feito pela classe utils

--- src/main.cpp
+++ src/main.cpp
@@ -1,96 +1,92 @@
 #include "jacobi.h"
+#include "utils.h"
 #include <iostream>
 #include <vector>
-#include <cmath>
-#include <cstdlib>
-#include <ctime>
-#include <iomanip>
 #include <chrono>
 
-extern void executar_jacobi_cuda(double *h_A, double *h_b, double *h_x_final, int n, double tol, int max_iter);
+using namespace std;
+
+extern void executar_jacobi_cuda( double *h_A, double *h_b, double *h_x_final,
+                                 int n, double tol, int max_iter);
+
 
 int main() {
+
     double epsilon = 1e-6;
     int max_iter = 5000;
-    int i, j;
-    double temp_inicial, temp_final, temp_total, 
-            speedup, eficiencia;
+
+
 
     // alterar tamanho da matriz
     int n = 100;
 
+    // Cria sistema linear
     vector<vector<double>> A(n, vector<double>(n, 0.0));
     vector<double> b(n, 0.0);
 
-    // Gera uma matriz diagonal dominante
-    // Cria todos os elementos da matriz com valor 1.0
-    // Add (soma de todos os elementos da linha + 1.0) nos elementos da diagonal principal
-    // valores de b = {2, 4, 8, 10, ...)
-    for (i = 0; i < n; i++) {
-            double soma_linha = 0.0;
-            for (j = 0; j < n; j++) {
-                if (i != j) {
-                    A[i][j] = 1.0; 
-                    soma_linha += A[i][j];
-                }
-            }
-            A[i][i] = soma_linha + 1.0; 
-            b[i] = (i + 1) * 2.0; 
-    }
+    gerar_matriz(A, b, n);
 
-    // sequencial
-    auto inicio_seq = std::chrono::high_resolution_clock::now();
+
+
+
+    // Execução Sequencial
+    auto inicio_seq = chrono::high_resolution_clock::now();
+
     auto resultado = jacobi_sequencial(A, b, max_iter, epsilon);
-    auto fim_seq = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> tempo_gasto_seq = fim_seq - inicio_seq;
-    temp_total = tempo_gasto_seq.count(); 
-    cout << "Tempo sequencial: " << temp_total << "s\n\n";
+
+    auto fim_seq = chrono::high_resolution_clock::now();
+
+    chrono::duration<double> tempo_gasto_seq = fim_seq - inicio_seq;
+
+    double temp_total = tempo_gasto_seq.count();
+
+    cout << "Tempo Sequencial: " << temp_total << "s\n\n";
 
 
-    // Paralela
+
+
+    // Preparação CUDA
     double *h_A = new double[n * n];
     double *h_b = new double[n];
     double *h_x_final = new double[n];
 
-    // inicia o host de A e b
-    // Obs: transforma a matriz em um vetor
-    for (int linha = 0; linha < n; linha++) {
-        h_b[linha] = b[linha];
-        for (int coluna = 0; coluna < n; coluna++) {
-            h_A[linha * n + coluna] = A[linha][coluna];
-        }
-    }
+    converter_matriz_para_vetor(A, b, h_A, h_b, n);
 
-    // executa e calcula o tempo do jacobi_cuda
-    auto inicio = std::chrono::high_resolution_clock::now();
+
+
+
+    // Execução CUDA
+    auto inicio_cuda = chrono::high_resolution_clock::now();
+
     executar_jacobi_cuda(h_A, h_b, h_x_final, n, epsilon, max_iter);
-    auto fim = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> tempo_gasto = fim - inicio;
-    temp_total = tempo_gasto.count(); 
-    cout << "Tempo Paralelo: " << temp_total << "s\n\n";
-    
 
-    double tempo_cuda = tempo_gasto.count();
+    auto fim_cuda = chrono::high_resolution_clock::now();
 
-    speedup = temp_seq / tempo_cuda;
+    chrono::duration<double> tempo_gasto_cuda = fim_cuda - inicio_cuda;
 
-    cout << "Speedup: "
-        << speedup
-        << endl;
+    double tempo_cuda = tempo_gasto_cuda.count();
 
-    cout << "Tempo CUDA: "
-     << tempo_gasto.count()
-     << "s\n";
+    cout << "Tempo CUDA: " << tempo_cuda << "s\n\n";
 
-    double diferenca = 0.0;
 
-    for (int i = 0; i < n; i++) {
-        diferenca += abs(resultado[i] - h_x_final[i]);
-    }
 
-    cout << "Diferenca CPU vs GPU: "
-        << diferenca
-        << endl;
+
+
+    // Speedup
+    double speedup = temp_total / tempo_cuda;
+
+    cout << "Speedup: " << speedup << endl;
+
+
+
+
+    // Diferença CPU vs GPU
+    double diferenca = calcular_diferenca(resultado, h_x_final, n);
+
+    cout << "Diferenca CPU vs GPU: " << diferenca << endl;
+
+
+
 
     delete[] h_A;
     delete[] h_b;
